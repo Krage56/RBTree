efd8010530f5f189ba3235b364db5df651b575f9
Krage56 <proskuryakov.i.s@edu.mirea.ru>
2020-06-13 13:51:47 +0300

    first commit

---
+++ RBTree.cpp
@@ -0,0 +1,5 @@
+//
+// Created by krage56 on 13.06.2020.
+//
+
+#include "RBTree.h"
---
+++ RBTree.h
@@ -0,0 +1,14 @@
+//
+// Created by krage56 on 13.06.2020.
+//
+
+#ifndef RED_BLACK_TREE_RBTREE_H
+#define RED_BLACK_TREE_RBTREE_H
+
+
+class RBTree {
+
+};
+
+
+#endif //RED_BLACK_TREE_RBTREE_H
