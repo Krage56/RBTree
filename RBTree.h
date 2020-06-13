f525032c0eecd84ffb6b23173011b1616fc3bb80
Krage56 <proskuryakov.i.s@edu.mirea.ru>
2020-06-13 14:30:25 +0300

    Node-class description

--- RBTree.h
+++ RBTree.h
@@ -5,9 +5,22 @@
 #ifndef RED_BLACK_TREE_RBTREE_H
 #define RED_BLACK_TREE_RBTREE_H
 
-
+template <typename ValueType, typename KeyType>
 class RBTree {
-
+    enum color{
+        red,
+        black
+    };
+    class Node{
+    public:
+        Node(const KeyType& key, const ValueType& value, Node* child_left = nullptr, Node* child_right = nullptr);
+        ~Node();
+        friend class RBTree;
+    private:
+        KeyType key;
+        ValueType value;
+        RBTree::color nodeColor = RBTree::color::red;//по умолчанию изначально вставляется красный потомок
+    };
 };
 
 
